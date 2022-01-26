a7ef62627af3848c565f6d11e8632e7f1caa0771
p-adams <ghusiyy@gmail.com>
2022-01-25 23:57:44 -0800

    split render out from creation

--- src/cljs_quick_start/core.cljs
+++ src/cljs_quick_start/core.cljs
@@ -1,9 +1,17 @@
 (ns cljs-quick-start.core)
 
-(defn create-node []
-    (let [app (.getElementById js/document "app")
-          p (.createElement js/document "p")]
-        (set! (.-innerHTML p) "meow")
-        (.appendChild app  p)))
 
-(create-node)
+(defonce app (.getElementById js/document "app"))
+
+(defn render [el]
+    (.appendChild app el))
+
+(defn p-node [t]
+    (let [el (.createElement js/document "p")]
+        (set! (.-innerHTML el) t)
+        el))
+
+
+(render (p-node "meow"))
+
+
