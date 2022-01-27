b9be8bdb1d00a98446dd40d3b7a765017344ff4d
p-adams <ghusiyy@gmail.com>
2022-01-26 21:10:25 -0800

    fmt

--- src/cljs_quick_start/core.cljs
+++ src/cljs_quick_start/core.cljs
@@ -2,14 +2,17 @@
     (:require react-dom))
 
 
-(defonce root (.createRoot js/ReactDOM (.getElementById js/document "app")))
+(defonce root
+    (.createRoot js/ReactDOM
+        (.getElementById js/document "app")))
 
 
 (defn app []
     (let [el (.createElement js/React "div" nil "meow")]
     el))
 
-(.render root (app))
+(.render root
+    (app))
 
 
 
