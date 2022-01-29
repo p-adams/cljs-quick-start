f0a2707d33a761eea804669deb9025dfb7e8535a
p-adams <ghusiyy@gmail.com>
2022-01-29 00:50:09 -0800

    add sablono templating

--- src/cljs_quick_start/core.cljs
+++ src/cljs_quick_start/core.cljs
@@ -1,17 +1,30 @@
 (ns cljs-quick-start.core
     (:require react-dom))
 
+;;(defn e (1)
+;; [el props & children]
+;;  (apply js/React.createElement el (clj->js props) children))
+
+
 
 (defonce root
     (.createRoot js/ReactDOM
         (.getElementById js/document "app")))
 
+(let [[c, s] (js/React.useState 0)]
+    (.createElement js/React "div" #js{:className "counter-app"}
+            (.createElement js/React "button"
+                {:onClick
+                    (fn []( s (inc c)))})
+                (str "count: " c)))
+
 (defn counter []
-    (.createElement js/React "div" #js{:className "counter-app"} nil))
+  (.createElement js/React "div" nil 0))
+
 
 
 (defn app []
-    (let [el (.createElement js/React "div" #js{:className "app-container"} "meow")]
+    (let [el (.createElement js/React "div" #js{:className "app-container"} (counter))]
     el))
 
 (.render root
